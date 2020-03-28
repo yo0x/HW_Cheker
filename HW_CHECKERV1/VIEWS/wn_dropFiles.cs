3c4b26240d697741438765849c375726969d1a4a
Yonor <6yonor@gmail.com>
2020-03-28 22:35:32 +0300

    v2

---
+++ HW_CHECKERV1/VIEWS/wn_dropFiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_CHECKERV1.VIEWS
+{
+    public partial class wn_dropFiles : Form
+    {
+        public wn_dropFiles()
+        {
+            InitializeComponent();
+        }
+    }
+}
